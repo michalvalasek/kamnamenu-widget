26ae8c6e44d355cdc3c93580191acc69b7cb757d
Michal Valasek <michal.valasek@gmail.com>
2014-11-26 23:54:22 +0100

    Update the gulpfile: Uglify the dist js file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
     return gulp.src('src/widget.jsx')
         .pipe(react())
+        .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
 
